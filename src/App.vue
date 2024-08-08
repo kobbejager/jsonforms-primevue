5eecf8334acb7c08c8ba7a867bdc44bdff3908f1
kobbejager <wfremout+github@gmail.com>
2024-08-08 18:30:28 +0200

    create vue app and integrate jsonforms

---
+++ src/App.vue
@@ -0,0 +1,128 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { JsonForms, type JsonFormsChangeEvent } from "@jsonforms/vue";
+import { vanillaRenderers } from "@jsonforms/vue-vanilla";
+
+const renderers = [
+    ...vanillaRenderers,
+    // here you can add custom renderers
+];
+
+const schema = {
+    properties: {
+        name: {
+            type: "string",
+            minLength: 1,
+            description: "The task's name"
+        },
+        description: {
+            title: "Long Description",
+            type: "string",
+        },
+        done: {
+            type: "boolean",
+        },
+        dueDate: {
+            type: "string",
+            format: "date",
+            description: "The task's due date"
+        },
+        rating: {
+            type: "integer",
+            maximum: 5,
+        },
+        recurrence: {
+            type: "string",
+            enum: ["Never", "Daily", "Weekly", "Monthly"]
+        },
+        recurrenceInterval: {
+            type: "integer",
+            description: "Days until recurrence"
+        },
+    },
+};
+
+const uischema = {
+    type: "HorizontalLayout",
+    elements: [
+        {
+            type: "VerticalLayout",
+            elements: [
+                {
+                    type: "Control",
+                    scope: "#/properties/name",
+                },
+                {
+                    type: "Control",
+                    scope: "#/properties/description",
+                    options: {
+                        multi: true,
+                    }
+                },
+                {
+                    type: "Control",
+                    scope: "#/properties/done",
+                },
+            ],
+        },
+        {
+            type: "VerticalLayout",
+            elements: [
+                {
+                    type: "Control",
+                    scope: "#/properties/dueDate",
+                },
+                {
+                    type: "Control",
+                    scope: "#/properties/rating",
+                },
+                {
+                    type: "Control",
+                    scope: "#/properties/recurrence",
+                },
+                {
+                    type: "Control",
+                    scope: "#/properties/recurrenceInterval",
+                },
+            ],
+        },
+    ],
+};
+
+export default defineComponent({
+    name: "App",
+    components: {
+        JsonForms,
+    },
+    data() {
+        return {
+            // freeze renderers for performance gains
+            renderers: Object.freeze(renderers),
+            data: {
+                name: "Send email to Adrian",
+                description: "Confirm if you have passed the subject\nHereby ...",
+                done: true,
+                recurrence: "Daily",
+                rating: 3,
+            },
+            schema,
+            uischema,
+        };
+    },
+    methods: {
+        onChange(event: JsonFormsChangeEvent) {
+            this.data = event.data;
+        },
+    }
+});
+</script>
+
+<template>
+    <img alt="Vue logo" src="./assets/logo.svg" width="75" height="75" />
+    <h1>JSON Forms Vue 3</h1>
+    <div>
+        <json-forms :data="data" :renderers="renderers" :schema="schema" :uischema="uischema" @change="onChange" />
+    </div>
+</template>
+
+<style></style>

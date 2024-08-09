b7d3a226210f4d21f787a1bbde85266e4c85e78d
kobbejager <wfremout+github@gmail.com>
2024-08-09 22:06:52 +0200

    better examples

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@ import { JsonForms } from "@jsonforms/vue"
 import { primeVueRenderers } from "./renderers"
 
 
-//import { entry as customRenderer } from './renderers/StringControlRenderer2.vue'
-
 const renderers = [
     ...primeVueRenderers,
     // here you can add custom renderers
@@ -16,35 +14,112 @@ const renderers = [
 ]
 
 const schema = {
+    type: "object",
+    required: ["string_field"],
     properties: {
-        name: {
+        string_field: {
+            title: "String field",
+            description: "A required text field between 1 and 10 characters",
             type: "string",
             minLength: 1,
-            description: "The task's name"
+            maxLength: 10
         },
-        description: {
-            title: "Long Description",
+        string_field_with_default: {
+            title: "String field with default value",
+            description: "An optional text field with a default value",
+            type: "string",
+            default: "Default value"
+        },
+        string_field_with_examples: {
+            title: "String field with examples",
+            description: "An optional text field with examples defined in the schema",
+            type: "string",
+            examples: ["Example 1", "Example 2", "Example 3"],
+        },
+        string_field_with_suggestions: {
+            title: "String field with suggestions",
+            description: "An optional text field with examples defined in the schema",
+            type: "string"
+        },
+        textarea_field1: {
+            title: "Textarea field 1",
+            description: "An optional text field defined as textarea in the schema",
+            type: "string",
+            format: "textarea"
+        },
+        textarea_field2: {
+            title: "Textarea field 2",
+            description: "An optional text field defined as textarea in the uischema",
             type: "string",
         },
-        done: {
-            type: "boolean",
+        textarea_field_with_default: {
+            title: "Textarea field with default value",
+            description: "An optional text field with a default value  in the schema",
+            type: "string",
+            format: "textarea",
+            default: "Default value"
         },
-        dueDate: {
+        number_field: {
+            title: "Number field",
+            description: "A number field restricted to values between 0 and 1",
+            type: "number",
+            minimum: 0,
+            maximum: 1
+        },
+        integer_field: {
+            title: "Integer field",
+            description: "An integer field restricted to values between 0 and 100",
+            type: "integer",
+            minimum: 0,
+            maximum: 100
+        },
+        integer_field_with_default: {
+            title: "Integer field with default value",
+            description: "An integer field with a default value",
+            type: "integer",
+            default: 10
+        },
+        boolean_field: {
+            title: "Boolean field",
+            description: "A boolean field, rendered as a checkbox",
+            type: "boolean"
+        },
+        boolean_field_with_default: {
+            title: "Boolean field with default value",
+            description: "A boolean field, rendered as a checkbox, with a default value",
+            type: "boolean",
+            default: true
+        },
+        date_field: {
+            title: "Date field",
+            description: "A date field",
             type: "string",
             format: "date",
-            description: "The task's due date"
         },
-        rating: {
-            type: "integer",
-            maximum: 5,
-        },
-        recurrence: {
+        time_field: {
+            title: "Time field",
+            description: "A time field",
             type: "string",
-            enum: ["Never", "Daily", "Weekly", "Monthly"]
+            format: "time",
         },
-        recurrenceInterval: {
-            type: "number",
-            description: "Days until recurrence"
+        date_time_field: {
+            title: "Date-time field",
+            description: "A date time field",
+            type: "string",
+            format: "date-time",
+        },
+        enum_field: {
+            title: "Enum field",
+            description: "An enum field",
+            type: "string",
+            enum: ["Option 1", "Option 2", "Option 3"]
+        },
+        enum_field_with_default: {
+            title: "Enum field with default value",
+            description: "An enum field with a default value",
+            type: "string",
+            enum: ["Option 1", "Option 2", "Option 3"],
+            default: "Option 1"
         },
     },
 }
@@ -57,52 +132,103 @@ const uischema = {
             elements: [
                 {
                     type: "Control",
-                    scope: "#/properties/name",
+                    scope: "#/properties/string_field",
                 },
                 {
-                    type: "Control",
-                    scope: "#/properties/description",
+                    type: "Control", 
+                    scope: "#/properties/string_field_with_default" 
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/string_field_with_examples" 
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/string_field_with_suggestions",
                     options: {
-                        multi: true,
+                        suggestion: [
+                            "Suggestion 1",
+                            "Suggestion 2",
+                            "Suggestion 3"
+                        ]
                     }
                 },
                 {
                     type: "Control",
-                    scope: "#/properties/done",
+                    scope: "#/properties/textarea_field1",
+                },
+                {
+                    type: "Control",
+                    scope: "#/properties/textarea_field2",
+                    options: {
+                        multi: true,
+                    }
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/textarea_field_with_default"
+                }
+            ],
+        },
+        {
+            type: "VerticalLayout",
+            elements: [
+                { 
+                    type: "Control", 
+                    scope: "#/properties/number_field"
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/integer_field"
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/integer_field_with_default"
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/boolean_field"
+                },
+                {
+                    type: "Control", 
+                    scope: "#/properties/boolean_field_with_default"
+                }
+            ],
+        },
+        {
+            type: "VerticalLayout",
+            elements: [
+                { 
+                    type: "Control", 
+                    scope: "#/properties/date_field"
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/date_time_field"
+                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/time_field"
                 },
             ],
         },
         {
             type: "VerticalLayout",
             elements: [
-                {
-                    type: "Control",
-                    scope: "#/properties/dueDate",
+                { 
+                    type: "Control", 
+                    scope: "#/properties/enum_field"
                 },
-                {
-                    type: "Control",
-                    scope: "#/properties/rating",
-                },
-                {
-                    type: "Control",
-                    scope: "#/properties/recurrence",
-                },
-                {
-                    type: "Control",
-                    scope: "#/properties/recurrenceInterval",
-                },
+                { 
+                    type: "Control", 
+                    scope: "#/properties/enum_field_with_default"
+                }
             ],
-        },
+        }
     ],
 }
 
-const defaultData = {
-    name: "Send email to Adrian",
-    description: "Confirm if you have passed the subject\nHereby ...",
-    done: true,
-    recurrence: "Daily",
-    rating: 3,
-}
+const defaultData = {}
 
 const state = reactive({
     renderers: Object.freeze(renderers),    // freeze the renderers for performance gains
@@ -119,45 +245,18 @@ const onChange = (event) => {
 <template>
     <div class="flex flex-col h-screen">
         <main class="container w-full m-12">
-
-            <h1 class="text-4xl text-black dark:text-white font-bold text-center">Tailwind CSS + PrimeVue</h1>
-
-            <!-- jsonforms example -->
+            <h1 class="text-4xl text-black dark:text-white font-bold text-center">PrimeVue + JsonForms</h1>
 
             <div>
                 <json-forms :data="state.data" :renderers="state.renderers" :schema="schema" :uischema="uischema" @change="onChange" />
             </div>
 
-            <pre>{{ state.data }}</pre>
-
-            <!-- primevue example -->
-
             <h2 class="py-6 text-2xl font-bold text-center">
-                Some PrimeVue/ Tailwind CSS tests
+                Form data
             </h2>
             
-            <Button icon="pi pi-home" aria-label="Save" class="mr-2" />
-            <Button label="Profile" icon="pi pi-user" class="mr-2"/>
-            <Button label="Save" icon="pi pi-check" iconPos="right" class="mr-2" />
-            <Button label="Search" icon="pi pi-search" iconPos="top" class="mr-2" />
-            <Button label="Update" icon="pi pi-refresh" iconPos="bottom" />
+            <pre>{{ state.data }}</pre>
 
-            <InputGroup class="my-2">
-                <Button label="Save" icon="pi pi-check" severity="success"/>
-                <Button label="Delete" icon="pi pi-trash" severity="danger" />
-                <Button label="Cancel" icon="pi pi-times" />
-            </InputGroup>
-
-            <Button label="Primary" outlined class="mr-2" />
-            <Button label="Danger" severity="danger" outlined />
-
-            <InputNumber 
-                inputId="integeronly"
-                showButtons 
-                d_modelValue="123"
-                fluid
-                class="p-2" 
-            />
         </main>
     </div>
 

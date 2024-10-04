c6aeb91ee79932344502baaf5ee89fc04954a7c2
kobbejager <wfremout+github@gmail.com>
2024-10-04 09:34:47 +0200

    adapt labelRenderer

--- src/renderers/label/LabelRenderer.vue
+++ src/renderers/label/LabelRenderer.vue
@@ -1,38 +1,39 @@
-<template>
-  <label v-if="label.visible" :class="styles.label.root">
-    {{ label.text }}
-  </label>
-</template>
-
 <script lang="ts">
 import {
-  JsonFormsRendererRegistryEntry,
-  LabelElement,
-  rankWith,
-  uiTypeIs,
+    JsonFormsRendererRegistryEntry,
+    LabelElement,
+    rankWith,
+    uiTypeIs,
 } from '@jsonforms/core';
 import { defineComponent } from 'vue';
 import {
-  rendererProps,
-  RendererProps,
-  useJsonFormsLabel,
+    rendererProps,
+    RendererProps,
+    useJsonFormsLabel,
 } from '@jsonforms/vue';
 import { usePrimeVueLabel } from '../util';
 
 const labelRenderer = defineComponent({
-  name: 'LabelRenderer',
-  props: {
-    ...rendererProps<LabelElement>(),
-  },
-  setup(props: RendererProps<LabelElement>) {
-    return usePrimeVueLabel(useJsonFormsLabel(props));
-  },
+    name: 'LabelRenderer',
+    props: {
+        ...rendererProps<LabelElement>(),
+    },
+    setup(props: RendererProps<LabelElement>) {
+        return usePrimeVueLabel(useJsonFormsLabel(props));
+    },
 });
 
 export default labelRenderer;
 
 export const entry: JsonFormsRendererRegistryEntry = {
-  renderer: labelRenderer,
-  tester: rankWith(1, uiTypeIs('Label')),
+    renderer: labelRenderer,
+    tester: rankWith(1, uiTypeIs('Label')),
 };
 </script>
+
+
+<template>
+    <label v-if="label.visible" :class="styles.label.root">
+        {{ label.text }}
+    </label>
+</template>

c69c637869b8df81cedfff9a1a917582244f2140
kobbejager <wfremout+github@gmail.com>
2024-11-19 15:57:37 +0100

    decorate ExamplesForm component

--- src/components/ExamplesForm.vue
+++ src/components/ExamplesForm.vue
@@ -8,6 +8,12 @@
     import { JsonForms } from "@jsonforms/vue"
     import { primeVueRenderers } from "@/renderers"
 
+    import Tabs from 'primevue/tabs';
+    import TabList from 'primevue/tablist';
+    import Tab from 'primevue/tab';
+    import TabPanels from 'primevue/tabpanels';
+    import TabPanel from 'primevue/tabpanel';
+
 /* 
     props
 */
@@ -37,24 +43,82 @@
 <template>
 
     <div class="w-full h-screen">
-        <div>
-            <JsonForms 
-                :data="state.data" 
-                :renderers="state.renderers" 
-                :schema="props.example.schema" 
-                :uischema="props.example.uiSchema" 
-                @change="onChange" 
-            />
-        </div>
+        <Tabs value="form">
+            <TabList>
+                <Tab value="form">Form</Tab>
+                <Tab value="schema">JSON Schema</Tab>
+                <Tab value="ui">UI Schema</Tab>
+                <Tab value="i18n">i18n Schema</Tab>
+                <Tab value="data">Data</Tab>
+            </TabList>
 
-        <h2 class="py-6 text-2xl font-bold text-center">
-            Form data
-        </h2>
+            <TabPanels>
+                <TabPanel value="form">
+                    <JsonForms 
+                        :data="state.data" 
+                        :renderers="state.renderers" 
+                        :schema="props.example.schema" 
+                        :uischema="props.example.uiSchema" 
+                        @change="onChange" 
+                    />
+                </TabPanel>
 
-        <pre>{{ props.example.schema }}</pre>
-        <pre>{{ state.data }}</pre>
+                <TabPanel value="schema">
+                    <div 
+                        class="card font-mono text-sm"
+                    >
+                        <pre>{{ props.example.schema }}</pre>
+                    </div>
+                </TabPanel>
+
+                <TabPanel value="ui">
+                    <div 
+                        v-if="props.example.uiSchema"
+                        class="card font-mono text-sm"
+                    >
+                        <pre>{{ props.example.uiSchema }}</pre>
+                    </div>
+                    <div 
+                        v-else 
+                        class="italic"
+                    >
+                        Auto generated UI schema
+                    </div>
+                </TabPanel>
+
+                <TabPanel value="i18n">
+                    <div 
+                        v-if="props.example.i18nSchema"
+                        class="card font-mono text-sm"
+                    >
+                        <pre>{{ props.example.i18nSchema }}</pre>
+                    </div>
+                    <div
+                        v-else
+                        class="italic"
+                    >
+                        No i18n schema
+                    </div>
+                </TabPanel>
+
+                <TabPanel value="data">
+                    <div 
+                        class="card font-mono text-sm"
+                    >
+                        <pre>{{ state.data }}</pre>
+                    </div>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     </div>
 
 </template>
 
-<style></style>
+<style scoped>
+.card {
+    background: #fff;
+    border: 1px solid var(--p-surface-200);
+    border-radius: 10px;
+    padding: 1rem;
+}
+</style>

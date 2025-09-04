c4e323f27efe0e08fcf5100b2d15b78a279e9a80
kobbejager <wfremout+github@gmail.com>
2025-09-04 19:10:15 +0200

    include stylesheet in build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { primeVueRenderers } from './renderers/renderers';
 export * from './renderers';
 
+// Include library CSS so consumers can import styles from the package
+import './style.css';
+

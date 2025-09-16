cf91db79c6f464d2bebf822e5802c37b7ebcf17a
kobbejager <wfremout+github@gmail.com>
2025-09-16 19:58:20 +0200

    add options for alternative rendering to EnumControl

--- src/renderers/controls/EnumControlRenderer.vue
+++ src/renderers/controls/EnumControlRenderer.vue
@@ -11,12 +11,16 @@ import { default as ControlWrapper } from "./ControlWrapper.vue";
 import { usePrimeVueControl } from "../util";
 
 import Select from "primevue/select";
+import RadioButton from "primevue/radiobutton";
+import SelectButton from "primevue/selectbutton";
 
 const controlRenderer = defineComponent({
     name: "EnumControlRenderer",
     components: {
         ControlWrapper,
         Select,
+        RadioButton,
+        SelectButton,
     },
     props: {
         ...rendererProps<ControlElement>(),
@@ -49,7 +53,38 @@ export const entry: JsonFormsRendererRegistryEntry = {
         :applied-options="appliedOptions"
         :show-errors="showErrors"
     >
+        <div v-if="appliedOptions.selectButton">
+            <SelectButton
+                :id="control.id + '-selectbutton'"
+                :model-value="control.data"
+                :options="control.options"
+                optionLabel="label"
+                optionValue="value"
+                :disabled="!control.enabled"
+                :class="styles.control.select"
+                :invalid="showErrors"
+                @update:model-value="onChange"
+                @focus="isFocused = true"
+                @blur="() => { isFocused = false; markTouched(); }"
+            />
+        </div>
+        <div v-else-if="appliedOptions.radio" :class="appliedOptions.horizontal ? 'flex flex-wrap gap-6 items-center' : 'flex flex-col gap-2'">
+            <div v-for="(opt, idx) in control.options" :key="idx" class="flex items-center gap-2">
+                <RadioButton
+                    :inputId="control.id + `-radio-${idx}`"
+                    :model-value="control.data"
+                    :value="opt.value"
+                    :disabled="!control.enabled"
+                    :invalid="showErrors"
+                    @update:model-value="onChange"
+                    @focus="isFocused = true"
+                    @blur="() => { isFocused = false; markTouched(); }"
+                />
+                <label :for="control.id + `-radio-${idx}`">{{ opt.label }}</label>
+            </div>
+        </div>
         <Select
+            v-else
             :id="control.id + '-select'"
             :model-value="control.data"
             :options="control.options"

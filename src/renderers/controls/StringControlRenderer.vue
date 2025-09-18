028fe17c70e138392777cb283566de2f2f0da39c
kobbejager <wfremout+github@gmail.com>
2025-09-18 13:46:49 +0200

    enhance StringControlRenderer with password and input mask options

--- src/renderers/controls/StringControlRenderer.vue
+++ src/renderers/controls/StringControlRenderer.vue
@@ -9,14 +9,19 @@ import { defineComponent } from "vue";
 import { rendererProps, useJsonFormsControl, RendererProps } from "@jsonforms/vue";
 import { default as ControlWrapper } from "./ControlWrapper.vue";
 import { usePrimeVueControl } from "../util";
+import type { StringControlOptions } from "../util";
 
 import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import InputMask from "primevue/inputmask";
 
 const controlRenderer = defineComponent({
     name: "StringControlRenderer",
     components: {
         ControlWrapper,
         InputText,
+        Password,
+        InputMask,
     },
     props: {
         ...rendererProps<ControlElement>(),
@@ -25,7 +30,7 @@ const controlRenderer = defineComponent({
         const adaptTarget = (value: any) => 
             value === "" ? undefined : value.toString();
 
-        return usePrimeVueControl(
+        return usePrimeVueControl<ReturnType<typeof useJsonFormsControl>, StringControlOptions>(
             useJsonFormsControl(props),
             adaptTarget
         );
@@ -48,7 +53,39 @@ export const entry: JsonFormsRendererRegistryEntry = {
         :applied-options="appliedOptions"
         :show-errors="showErrors"
     >
+        <Password
+            v-if="appliedOptions.password"
+            :id="control.id + '-password'"
+            :model-value="control.data"
+            fluid
+            :class="styles.control.input"
+            :disabled="!control.enabled"
+            :autofocus="appliedOptions.focus"
+            :placeholder="appliedOptions.placeholder"
+            :feedback="false"
+            :toggleMask="appliedOptions.toggleMask ?? true"
+            :invalid="showErrors"
+            @update:model-value="onChange"
+            @focus="isFocused = true"
+            @blur="() => { isFocused = false; markTouched(); }"
+        />
+        <InputMask
+            v-else-if="appliedOptions.inputMask"
+            :id="control.id + '-mask'"
+            :model-value="control.data"
+            fluid
+            :class="styles.control.input"
+            :disabled="!control.enabled"
+            :autofocus="appliedOptions.focus"
+            :placeholder="appliedOptions.placeholder ?? appliedOptions.inputMask"
+            :mask="appliedOptions.inputMask"
+            :invalid="showErrors"
+            @update:model-value="onChange"
+            @focus="isFocused = true"
+            @blur="() => { isFocused = false; markTouched(); }"
+        />
         <InputText
+            v-else
             :id="control.id + '-input'"
             :model-value="control.data"
             fluid

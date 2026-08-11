50581f285620533023c56070dac744810cc3c076
wim <wim.git.kevcy@simplelogin.com>
2026-08-11 09:48:19 +0200

    fix bug in DateControlRenderer: months start counting from 0

--- src/renderers/controls/DateControlRenderer.vue
+++ src/renderers/controls/DateControlRenderer.vue
@@ -12,6 +12,23 @@ import { usePrimeVueControl } from "../util";
 
 import DatePicker from "primevue/datepicker";
 
+const DATE_ONLY_PATTERN = /^(\d{4})-(\d{2})-(\d{2})$/;
+
+const parseDateOnly = (value: string): Date | undefined => {
+    const match = DATE_ONLY_PATTERN.exec(value);
+    if (!match) return undefined;
+    const year = Number(match[1]);
+    const month = Number(match[2]);
+    const day = Number(match[3]);
+    return new Date(year, month - 1, day);
+};
+
+const formatDateOnly = (value: Date): string =>
+    value.getFullYear() +
+    "-" +
+    ("0" + (value.getMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + value.getDate()).slice(-2);
 
 const controlRenderer = defineComponent({
     name: "DateControlRenderer",
@@ -24,10 +41,8 @@ const controlRenderer = defineComponent({
     },
     setup(props: RendererProps<ControlElement>) {
         const jsDate = ref();
-        const adaptTarget = (value: any) => 
-            value instanceof Date 
-                ? value.getFullYear() + "-" + ("0" + value.getMonth()).slice(-2) + "-" + ("0" + value.getDate()).slice(-2) 
-                : undefined;
+        const adaptTarget = (value: any) =>
+            value instanceof Date ? formatDateOnly(value) : undefined;
         const control = usePrimeVueControl(
             useJsonFormsControl(props),
             adaptTarget
@@ -36,8 +51,11 @@ const controlRenderer = defineComponent({
         // set initial value
         onMounted(() => {
             const value = control.control.value.data;
-            if (value !== undefined && value !== null) {
-                jsDate.value = new Date(value);
+            if (typeof value === "string") {
+                const parsed = parseDateOnly(value);
+                if (parsed !== undefined) {
+                    jsDate.value = parsed;
+                }
             }
         });
 

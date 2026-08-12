2e4ee369e4d7b0b5deb16b43fc9ff4a71c19f157
kobbejager <wfremout+github@gmail.com>
2026-08-12 16:32:53 +0200

    bump version: v0.8.0

--- dist-demo/assets/javascript-Be-PLOSD.js
+++ dist-demo/assets/javascript-Be-PLOSD.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-C3gf4bDs.js";import"./index-V6dtrbno.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-D8nMfDjK.js";import"./index-6yG0knsL.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.54.0(7c2310116c57517348bbd868a21139f32454be22)
  * Released under the MIT license
